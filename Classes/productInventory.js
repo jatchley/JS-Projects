011732bf28d11ce1bb4822f42a678bbd17b8ff6b
Josh Atchley <jatchley@wesleyan.edu>
2014-06-06 17:52:57 -0500

    Product inventory project

--- Classes/productInventory.js
+++ Classes/productInventory.js
@@ -1,4 +1,4 @@
-// Product Inventory Project – Create an application which manages an inventory of products. Create a product class which has a price, id, and quantity on hand. Then create an inventory class which keeps track of various products and can sum up the inventory value.
+// Product Inventory Project – Create an application which manages an inventory of products. Create a product class which has a price, id, and quantity on hand. Then create an inventory class which keeps track of various products and can sum up the inventory value
 
 // Product constructor
 function Product(name, price, quantity, measurement) {
