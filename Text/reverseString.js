54d37f4ef94d1e9905b6e28aa8aaf54db6113732
Josh Atchley <jatchley@wesleyan.edu>
2014-06-05 23:25:31 -0500

    Revert "Reverse string function in "Text" problem set"
    This reverts commit 65784699e5e97096c28697ce138bb0346762a0bb.

--- Text/reverseString.js
+++
@@ -1,8 +0,0 @@
-//Reverse a String – Enter a string and the program will reverse it and print it out.
-
-function reverse(string) {
-	return string.split("").reverse().join("");
-}
-
-// test case
-console.log(reverse("Hello world!"));
