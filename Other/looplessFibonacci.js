b194b80e920644160a74aa4d582ef63c33402ae5
Josh Atchley <jatchley@wesleyan.edu>
2014-06-14 20:45:21 -0500

    Finds Nth Fibonacci number without loops/arrays

--- Other/looplessFibonacci.js
+++ Other/looplessFibonacci.js
@@ -1,31 +1,22 @@
 // Calculate the 35th Fibonacci number (9,227,465) without using an array or a loop.
 
-var count = 0;
 var firstNum = 0;
 var secondNum = 1;
 var answer = firstNum + secondNum;
 
 // The attempt
 function sequence(target) {
-	if (target <= 1) {
-		console.log(1);
-	} else if (count < target) {
+	if (target < 1) {
+		console.log(null);
+	} else {
 		console.log(answer);
 		secondNum = firstNum;
 		firstNum = answer;
 		answer = firstNum + secondNum;
-		count ++;
+		target--;
+		sequence(target);
 	} 
 
 }
 
 sequence(35);
-
-// While loop that works properly
-while (count < 35) {
-	console.log(answer);
-		secondNum = firstNum;
-		firstNum = answer;
-		answer = firstNum + secondNum;
-		count ++;
-}
